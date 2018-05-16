04cb6bf5b84dde325ebfd228230af41a717639b1
EC2 Default User <ec2-user@ip-172-31-38-174.us-west-2.compute.internal>
2018-05-16 20:34:29 +0000

    added routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+  root to: 'pages#contact'
+  root to: 'pages#about'
+  #get '/index' => 'pages#index'
+  #get '/contact' => 'pages#contact'
+  #get '/about' => 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
